5e9fa9e4db2d603aeab6ea9dac40fd8b96c343b9
Craig Chambers <cchambers@salisbury6c.ac.uk>
2020-09-25 10:18:19 +0100

    Working demo

--- test/index.js
+++ test/index.js
@@ -1,15 +1,11 @@
 var fname = document.querySelector("#fname")
-console.log(fname)
-
+var sname = document.querySelector("#lname")
 var button = document.querySelector("#test")
-console.log(button)
 var label = document.querySelector("#label")
 
-
-document.addEventListener("load", function(){
+window.addEventListener("load", function(){
     button.addEventListener("click", function() {
-        label.innerHTML = fname.value
-
+        label.innerHTML = "Hello " + fname.value + " " + lname.value
     })
 })
 
